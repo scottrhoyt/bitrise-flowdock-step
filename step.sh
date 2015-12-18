014610a75bc112f7ac0d2e008a3d94bb6a6740d6
Scott Hoyt <scott@wildonion.io>
2015-12-17 17:57:50 -0800

    Added setup to shell script.

--- step.sh
+++ step.sh
@@ -4,14 +4,26 @@ THIS_SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source "${THIS_SCRIPTDIR}/_bash_utils/utils.sh"
 source "${THIS_SCRIPTDIR}/_bash_utils/formatted_output.sh"
 
-# init / cleanup the formatted output
+# ------------------------------
+# --- Init Output
+
 echo "" > "${formatted_output_file_path}"
+
+# ---------------------
+# --- Required Inputs
 
 if [ -z "${flowdock_source_token}" ] ; then
 	write_section_to_formatted_output "# Error"
 	write_section_start_to_formatted_output '* Required input `$flowdock_source_token` not provided!'
 	exit 1
 fi
+
+# ---------------------
+# --- Main
+
+write_section_to_formatted_output "# Setup"
+bash "${THIS_SCRIPT_DIR}/_setup.sh"
+fail_if_cmd_error "Failed to setup the required tools!"
 
 resp=$(ruby "${THIS_SCRIPTDIR}/flowdock.rb")
 ex_code=$?
